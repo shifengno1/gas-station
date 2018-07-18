def5e4820d22f05467719b276127ac09fb0f2aee
SeanLIUPAN <liupan@sari.ac.cn>
2018-07-18 16:43:32 +0800

    修改充值流水-刘盼0718

--- src/reducers/rechargeListReducer.js
+++ src/reducers/rechargeListReducer.js
@@ -6,13 +6,16 @@ const defaultState = {
         rows: [],
         pageNo: 0,
     },
+    allMoney: '',
 };
 
 export default createReducer(defaultState, {
     [constant.RECHARGE_LIST_DATA]: (state, action) => {
+        console.log('qqqq', action);
         return {
             ...state,
             listData: action.data.data,
+            allMoney: action.data.data.data,
         };
     },
 });
