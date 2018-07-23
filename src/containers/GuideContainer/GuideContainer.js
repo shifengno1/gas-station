967b8f1ca6a40c130b6dc3ada3f17f74841774f3
SeanLIUPAN <liupan@sari.ac.cn>
2018-07-23 16:47:11 +0800

    修改主页-增加九宫格图表-刘盼-0723

--- src/containers/GuideContainer/GuideContainer.js
+++ src/containers/GuideContainer/GuideContainer.js
@@ -20,78 +20,78 @@ export default class GuideContainer extends Component {
                     <div className="square-row">
                         <div className="square-item">
                             <div className="item-top">
-                                123
+                                <img src={require('./img/充值.png')} alt="充值流水" className="imgs" />
                             </div>
                             <div className="item-bottom">
-                                123
+                                会员充值记录
                             </div>
                         </div>
                         <div className="square-item">
                             <div className="item-top">
-                                123
+                                <img src={require('./img/库存，堆积.png')} alt="库存，堆积" className="imgs" />
                             </div>
                             <div className="item-bottom">
-                                123
+                                进销存
                             </div>
                         </div>
                         <div className="square-item">
                             <div className="item-top">
-                                123
+                                <img src={require('./img/资金流水明细-变.png')} alt="资金流水明细" className="imgs" />
                             </div>
                             <div className="item-bottom">
-                                123
-                            </div>
-                        </div>
-                    </div>
-                    <div className="square-row">
-                        <div className="square-item">
-                            <div className="item-top">
-                                123
-                            </div>
-                            <div className="item-bottom">
-                                123
-                            </div>
-                        </div>
-                        <div className="square-item">
-                            <div className="item-top">
-                                123
-                            </div>
-                            <div className="item-bottom">
-                                123
-                            </div>
-                        </div>
-                        <div className="square-item">
-                            <div className="item-top">
-                                123
-                            </div>
-                            <div className="item-bottom">
-                                123
+                                交易记录
                             </div>
                         </div>
                     </div>
                     <div className="square-row">
                         <div className="square-item">
                             <div className="item-top">
-                                123
+                                <img src={require('./img/卡片.png')} alt="IC卡片" className="imgs" />
                             </div>
                             <div className="item-bottom">
-                                123
+                                IC卡
                             </div>
                         </div>
                         <div className="square-item">
                             <div className="item-top">
-                                123
+                                <img src={require('./img/优惠券.png')} alt="优惠活动" className="imgs" />
                             </div>
                             <div className="item-bottom">
-                                123
+                                优惠活动
                             </div>
                         </div>
                         <div className="square-item">
                             <div className="item-top">
-                                123
+                                <img src={require('./img/定位.png')} alt="站点" className="imgs" />
                             </div>
                             <div className="item-bottom">
-                                123
+                                我的站点
+                            </div>
+                        </div>
+                    </div>
+                    <div className="square-row">
+                        <div className="square-item">
+                            <div className="item-top">
+                                <img src={require('./img/主页.png')} alt="个人中心" className="imgs" />
+                            </div>
+                            <div className="item-bottom">
+                                个人中心
+                            </div>
+                        </div>
+                        <div className="square-item">
+                            <div className="item-top">
+                                <img src={require('./img/我的.png')} alt="我的" className="imgs" />
+                            </div>
+                            <div className="item-bottom">
+                                我的会员
+                            </div>
+                        </div>
+                        <div className="square-item">
+                            <div className="item-top">
+                                <img src={require('./img/修改密码.png')} alt="修改密码" className="imgs" />
+                            </div>
+                            <div className="item-bottom">
+                                修改密码
                             </div>
                         </div>
                     </div>
